49203f5541991307e24adc7fb784ffb6b711f705
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-01-10 16:20:00 +0530

    Time: 0 ms (100%), Space: 10.3 MB (51.61%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int n = s.size() - 1;
+        int i = 0;
+
+        while (i < n) {
+            if ((s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+                 s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')) {
+                if ((s[n] == 'a' || s[n] == 'e' || s[n] == 'i' || s[n] == 'o' || s[n] == 'u' ||
+                     s[n] == 'A' || s[n] == 'E' || s[n] == 'I' || s[n] == 'O' || s[n] == 'U')) {
+                    swap(s[i], s[n]);
+                    n--;
+                    i++;
+                } else {
+                    n--; 
+                }
+            } else {
+                i++; // Move the 'i' pointer to check the next character from the beginning
+            }
+        }
+
+        return s;
+    }
+};
