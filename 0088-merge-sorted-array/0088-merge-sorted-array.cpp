76313b760eecfd104a2bc3e9287790a1f87e98d6
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-03-29 13:42:36 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.91%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // Clear extra elements in nums1 beyond the valid portion
-        for (int i = nums1.size() - 1; i >= m; i--) {
-            nums1.pop_back();
+        for (int j = 0, i = m; j<n; j++){
+            nums1[i] = nums2[j];
+            i++;
         }
-
-        // Add the valid elements from nums2 to nums1
-        for (int i = 0; i < n; i++) {
-            nums1.push_back(nums2[i]);
-        }
-
-        // Sort nums1 to merge the two arrays
-        sort(nums1.begin(), nums1.end());
+        sort(nums1.begin(),nums1.end());
     }
 };
