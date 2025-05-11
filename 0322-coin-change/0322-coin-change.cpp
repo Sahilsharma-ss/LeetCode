ad7404074306f7ca88f893297c4ae750dbc2a9e5
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-05-11 16:41:18 +0530

    Time: 45 ms (37.04%), Space: 49.3 MB (31.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,1e8);
-        dp[0]=0;
-        for(int i=0;i<=amount;i++)
+    int f(int ind,int T,vector<int>&coins,vector<vector<int>>&dp)
+    {
+        if(ind==0)
         {
-            for(auto c : coins)
-            {
-                if(i-c>=0)
-                {
-                    dp[i]=min(dp[i],dp[i-c] + 1);
-                }
-            }
-        
+            if(T%coins[0]==0) return T/coins[0];
+            return 1e9;
         }
-        return dp[amount]==1e8? -1:dp[amount] ;
+        if(dp[ind][T]!=-1) return dp[ind][T];
+        int not_take=0+f(ind-1,T,coins,dp);
+        int take=1e9;
+        if(coins[ind]<=T)
+        {
+            take=1+f(ind,T-coins[ind],coins,dp);
+        }
+        return dp[ind][T]=min(take,not_take);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+       int n=coins.size();
+       vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+       int res=f(n-1,amount,coins,dp);
+       if(res==1e9) return -1;
+       return res;
     }
 };
