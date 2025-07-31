575ba3ec48298f8707e8fac55b04eb55f8e07523
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-07-31 21:41:42 +0530

    Time: 55 ms (98.44%), Space: 63 MB (98.82%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-    int n;
+    int n, N;
     inline bool inside(int i, int j){
         return 0<=i && i<n && 0<=j && j<n;
     }
     inline int idx(int i,  int j){
         return i*n+j;
     }
+    const int d[5]={0, 1, 0, -1, 0};
+    vector<int> viz;// -1 for unvisited, other for component number
+
+    int dfs(int i, int j, int comp, vector<vector<int>>& grid) {
+        viz[idx(i, j)] = comp;
+        int cnt = 1;
+        for (int a=0; a < 4; a++) {
+            int r=i+d[a], s=j+d[a+1];
+            if (inside(r, s) && grid[r][s]==1 && viz[idx(r, s)]==-1)
+                cnt+= dfs(r, s, comp, grid);
+        }
+        return cnt;
+    }
+
     int largestIsland(vector<vector<int>>& grid) {
-        n=grid.size();
-        const int N=n*n;
-        const int d[5]={0,1,0,-1,0};
-        UnionFind G(N);
-        int maxSz=1;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if (grid[i][j]){
-                    int a=idx(i, j), b;
-                    int D=(i<n-1)?grid[i+1][j]:0, R=(j<n-1)?grid[i][j+1]:0;
-                    if (D>0){// down
-                        b=a+n;
-                        if (G.Union(a, b))// connect 2 components
-                            maxSz=max(maxSz, G.Size[G.Find(a)]);
-                    }
-                    if (R>0){// right
-                        b=a+1;
-                        if (G.Union(a, b))// connect 2 components
-                            maxSz=max(maxSz, G.Size[G.Find(a)]);
-                    }
+        n = grid.size();
+        N = n*n;
+        viz.assign(N, -1);
+        
+        int maxSz=1, comp=0;
+        vector<int> Size; // size for component[comp]  
+
+        // Find components
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j]==1 && viz[idx(i, j)]==-1) {
+                    int cnt=dfs(i, j, comp++, grid);
+                    Size.push_back(cnt);
+                    maxSz=max(maxSz, cnt);
                 }
             }
         }
+
+        // 2nd loop
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if (grid[i][j]==0){
@@ -39,14 +49,14 @@ public:
                     for( int a=0; a<4; a++){
                         const int r=i+d[a], s=j+d[a+1];
                         if (inside(r, s) && grid[r][s]!=0)
-                            component[cz++]=G.Find(idx(r, s));
+                            component[cz++]=viz[idx(r, s)];
                     }
                     if (cz==0) continue;
                     sort(component, component+cz);
-                    int sz=1+G.Size[component[0]];
+                    int sz=1+Size[component[0]];
                     for (int k=1; k<cz; k++)
                         if (component[k]!=component[k-1]) 
-                            sz+=G.Size[component[k]];
+                            sz+=Size[component[k]];
                     maxSz=max(maxSz, sz);
                 }
             }
@@ -54,10 +64,6 @@ public:
         return maxSz;
     }
 };
-
-
-
-
 auto init = []() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
