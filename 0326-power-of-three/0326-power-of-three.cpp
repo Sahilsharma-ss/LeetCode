5953b8c6654cc3d4677e906219c99770d8251bbb
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-08-13 08:55:33 +0530

    Time: 0 ms (100%), Space: 8.9 MB (76.82%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        int i=0;
-        set<int>s;
-        while(pow(3,i)<=INT_MAX)
-        {
-            s.insert(pow(3,i));
-            i++;
-        }
-        if(s.find(n)!=s.end())
-        {
-            return true;
-        }
-        return false;
+    return n > 0 && 1162261467 % n == 0;
     }
 };
