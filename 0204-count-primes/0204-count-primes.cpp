6be4f800e59804d561df8cd95b2c7b81e3d73d63
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-08-21 23:14:28 +0530

    Time: 166 ms (75.23%), Space: 132.7 MB (9.99%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,16 +1,33 @@
 class Solution {
-public:
-    int countPrimes(int n) {
-        int cnt = 0;
-        vector<int> check(n+1, 1);
-        for(int i=2; i<n; i++) {
-            if(check[i] == 1) {
-                cnt++;
-                for(long long j=(long long)i*i; j<n; j+=i) {
-                    check[j] = 0;
-                }
+void solve(vector<int>&vec,int n)
+{
+    for(int i=2;i<n;i++)
+    {
+        vec[i]=1;
+    }
+    for(int i=2;i*i<n;i++)
+    {
+        if(vec[i]==1)
+        {
+            for(int j=i*i;j<n;j+=i)
+            {
+                vec[j]=0;
             }
         }
-        return cnt;
+    }
+}
+public:
+    int countPrimes(int n) {
+        vector<int>vec(n);
+        solve(vec,n);
+    int cnt=0;
+    for(int i=2;i<n;i++)
+    {
+        if(vec[i]==1)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
     }
 };
