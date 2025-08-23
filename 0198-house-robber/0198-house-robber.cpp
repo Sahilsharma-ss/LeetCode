b5a2b42cb1289eed110d899cfe60343e7f8cda72
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-08-23 19:46:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int dp[101];
-    int solve(vector<int>nums,int i,int n)
+    int solve(int ind,vector<int>&nums,vector<int>&dp)
     {
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int steal=nums[i]+solve(nums,i+2,n);
-        int skip=solve(nums,i+1,n);
-        return dp[i]=max(steal,skip);
+        if(ind==0)
+        {
+            return nums[ind];
+        }
+        if(dp[ind]!=-1) return dp[ind];
+        int not_take=0+solve(ind-1,nums,dp);
+        int take=-1e9;
+        if(ind-2>=0) take=solve(ind-2,nums,dp)+nums[ind];
+        return dp[ind]=max(take,not_take);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0,n);
+        vector<int>dp(n+1,-1);
+        return solve(n-1,nums,dp);
     }
 };
