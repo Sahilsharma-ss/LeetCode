cded8ba5c67e58bdd46659967a24cab2ea04e224
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-08-30 11:11:28 +0530

    Time: 3 ms (32.71%), Space: 16.1 MB (7.5%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp)
+    int n;
+    int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>&dp)
     {
-        if(j<0 || j>=grid[0].size()) return 1e9;
-        if(i==0) return grid[i][j];
+        if(j>=n||j<0) return 1e9;
+        if(i==n-1)
+        {
+            return matrix[i][j];
+        }
         if(dp[i][j]!=1e9) return dp[i][j];
-        int s=grid[i][j]+solve(i-1,j-1,grid,dp);
-        int ld=grid[i][j]+solve(i-1,j,grid,dp);
-        int rd=grid[i][j]+solve(i-1,j+1,grid,dp);
-        return dp[i][j]= min(s,min(ld,rd));
+        int left=matrix[i][j]+solve(i+1,j-1,matrix,dp);
+        int down=matrix[i][j]+solve(i+1,j,matrix,dp);
+        int right=matrix[i][j]+solve(i+1,j+1,matrix,dp);
+        return dp[i][j]=min({left,down,right});
+
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int mini=INT_MAX;
-        vector<vector<int>>dp(m,vector<int>(n,1e9));
+        n=matrix.size();
+        vector<vector<int>>dp(n,vector<int>(n,1e9));
+        int mini=1e9;
         for(int i=0;i<n;i++)
         {
-            mini=min(mini,solve(m-1,i,matrix,dp));
+           mini=min(mini,solve(0,i,matrix,dp));
         }
         return mini;
     }
