2816e03ec9350a0cc685ea5d452bd2042dc0fc72
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-09-02 12:04:12 +0530

    Time: 395 ms (5.19%), Space: 122.2 MB (5.19%) - LeetHub

---
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool nopoint(int a ,int b,int c,int d,vector<vector<int>>& points)
+    {
+        for(auto i : points)
+        {
+            int x=i[0];
+            int y=i[1];
+            if(x==a&&y==b || x==c&&y==d) continue;
+            if(x>=a&&x<=c && y<=b&&y>=d)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n=points.size();
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int a=points[i][0];
+                int b=points[i][1];
+                int c=points[j][0];
+                int d=points[j][1];
+                if (i == j) continue;
+                if (i == j) continue;
+                if (((a < c && b >= d) || (a == c && b > d)) && nopoint(min(a,c), max(b,d), max(a,c), min(b,d), points)) {
+                    cnt++;
+                }
+
+
+            }
+        }
+        return cnt;
+    }
+};
