e23812ba36e7988b8de2a8e3b68bb95bb3d00b1d
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-09-11 17:46:57 +0530

    Time: 23 ms (88.52%), Space: 26.9 MB (19.99%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,50 +1,59 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string s1, string s2) {
-        
-        int m = s1.length();
-        int n = s2.length();
-        
-        vector<vector<int>> t(m+1, vector<int>(n+1));
-        
-        for(int i = 0; i<m+1; i++) {
-            for(int j = 0; j < n+1; j++) {
-                if(i == 0 || j == 0)
-                    t[i][j] = i+j;
-                else if(s1[i-1] == s2[j-1])
-                    t[i][j] = 1 + t[i-1][j-1];
-                else
-                    t[i][j] = 1 + min(t[i-1][j], t[i][j-1]);
+    void lcs(int n,int m,string str1,string str2,vector<vector<int>>&dp)
+    {
+      for(int i=1;i<=n;i++)
+      {
+        for(int j=1;j<=m;j++)
+        {
+            if(str1[i-1]==str2[j-1])
+            {
+                dp[i][j] = 1+dp[i-1][j-1];
+            }
+            else
+            {
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
-                
-        string scs = "";
-        int i = m,  j = n;
-        while(i > 0 && j > 0) {
-            if(s1[i-1] == s2[j-1]) {
-                scs.push_back(s1[i-1]);
+      }
+    }
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n=str1.size();
+        int m=str2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        lcs(n,m,str1,str2,dp);
+
+        string str;
+        int i=n;
+        int j=m;
+        while(i>0 && j>0)
+        {
+            if(str1[i-1]==str2[j-1])
+            {
+                str+=str1[i-1];
                 i--;
                 j--;
-            } else {
-                if(t[i-1][j] < t[i][j-1]) {
-                    scs.push_back(s1[i-1]);
-                    i--;
-                } else {
-                    scs.push_back(s2[j-1]);
-                    j--;
-                }
+            }
+            else if(dp[i-1][j]>dp[i][j-1])
+            {
+                str+=str1[i-1];
+                i--;
+            }
+            else
+            {
+                str+=str2[j-1];
+                j--;
             }
         }
-        while(i > 0) {
-            scs.push_back(s1[i-1]);
+        while(i>0) {
+            str+=str1[i-1];
             i--;
         }
-        while(j > 0) {
-            scs.push_back(s2[j-1]);
+        while(j>0) {
+            str+=str2[j-1];
             j--;
         }
-        reverse(scs.begin(), scs.end());
-        
-        return scs;
+        reverse(str.begin(),str.end());
+        return str;
     }
 };
