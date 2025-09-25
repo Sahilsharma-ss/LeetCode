d0b537b835f2be1a2a18c9ec165d29743c2d88ba
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-09-25 10:43:56 +0530

    Time: 0 ms (100%), Space: 41.2 MB (40.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,18 +1,51 @@
 class Solution {
 public:
+    int productwithoutzero(vector<int>&nums)
+    {
+        int prod=1;
+        for(auto i : nums)
+        {
+            if(i!=0)
+            {
+                prod*=i;
+            }
+        }
+        return prod;
+    }
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n, 1);
-        int prefix = 1;
-        for (int i = 0; i < n; i++) {
-            ans[i] = prefix;
-            prefix *= nums[i];
+        int prod=1;
+        int n=nums.size();
+        bool zero=false;
+        int zerocnt=0;
+        vector<int>haszero(n,0);
+        for(int i=0;i<n;i++)
+        {
+            prod*=nums[i];
+            if(nums[i]==0)
+            {
+                zero=true;
+                haszero[i]=1;
+                zerocnt++;
+            }
         }
-        int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] *= suffix;
-            suffix *= nums[i];
+        vector<int>res(n,0);
+        if(!zero)
+        {
+            for(int i=0;i<n;i++)
+            {
+                res[i]=prod/nums[i];
+            }
         }
-        return ans;
+        else if(zerocnt==1)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(haszero[i]==1)
+                {
+                    res[i]=productwithoutzero(nums);
+                }
+            }
+        }
+        return res;
     }
 };
