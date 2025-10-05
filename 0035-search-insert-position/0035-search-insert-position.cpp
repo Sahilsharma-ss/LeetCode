4cc4fa81f5a9f7289593b93d9edf52249212178f
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-10-05 15:16:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,51 +1,27 @@
-
-
-
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-       
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0;
+        int r=n-1;
+        int ans=0;
+        while(l<r)
+        {
+            int mid=l+(r-l)/2;
+            // while(arr[mid]<target)
+            // {
+            //     mid++;
+            //     ans=mid+1;
+            // }
+            if(nums[mid]<target)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
             }
         }
-
-     
-        int[] newNums = new int[nums.length + 1];
-
-       
-        for (int i = 0; i < nums.length; i++) {
-            newNums[i] = nums[i];
-        }
-
-        newNums[nums.length] = target;
-
-        
-        insertionSort(newNums);
-
-        
-        for (int i = 0; i < newNums.length; i++) {
-            if (newNums[i] == target) {
-                return i;
-            }
-        }
-
-      
-        return -1;
+        return r+1;
     }
-
-    public void insertionSort(int[] nums) {
-        int n = nums.length;
-        for (int i = 1; i < n; i++) {
-            int key = nums[i];
-            int j = i - 1;
-
-           
-            while (j >= 0 && nums[j] > key) {
-                nums[j + 1] = nums[j];
-                j = j - 1;
-            }
-            nums[j + 1] = key;
-        }
-    }
-}
+};
