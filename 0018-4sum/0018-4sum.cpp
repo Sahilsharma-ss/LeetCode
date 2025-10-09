03916731e73221a851aa83fb456aa1379c3532dd
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-10-09 12:03:41 +0530

    Time: 347 ms (7.03%), Space: 83.1 MB (6.16%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> ans;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
+        set<vector<int>>st;
+        for(int i =n-1;i>=0;i--)
         {
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n;j++)
+            for(int j=i-1;j>=0;j--)
             {
-                if(j!=(i+1) && nums[j]==nums[j-1]) continue;
-                int k=j+1;
-                int l=n-1;
+                if(i==j) continue;
+                int k=0;
+                int l=j-1;
                 while(k<l)
                 {
-                    long long sum= nums[i];
-                    sum +=nums[j];
-                    sum +=nums[k];
-                    sum +=nums[l];
+                    long long sum =(long long) nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum==target)
                     {
-                        vector<int>temp = {nums[i],nums[j],nums[k],nums[l]};
-                        ans.push_back(temp);
-                        k++;
-                        l--;
-                        while(k<l && nums[k]==nums[k-1]) k++;
-                        while(k<l && nums[l]==nums[l+1]) l--;
+                        vector<int>vec{nums[i],nums[j],nums[k],nums[l]};
+                        sort(vec.begin(),vec.end());
+                        st.insert(vec);
                     }
-                    else if(sum<target) k++;
-                    else l--;
+                    if(sum<target)
+                    {
+                        k++;
+                    }
+                    else
+                    {
+                        l--;
+                    }
                 }
+                
             }
         }
-        return ans;
+        vector<vector<int>>res;
+        for(auto i : st)
+        {
+            res.push_back(i);
+        }
+        return res;
     }
 };
