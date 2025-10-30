4f6bcc8fea2837d1fae690371b2f297c1ed443bb
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-10-30 23:16:54 +0530

    Time: 7 ms (6.46%), Space: 15.8 MB (46.94%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        sort(nums.begin(), nums.end());  // ✅ Must sort first!
-
-        vector<long long> p(n), s(n);
-        p[0] = nums[0];
-        for (int i = 1; i < n; i++)
-            p[i] = p[i - 1] + nums[i];
-
-        s[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--)
-            s[i] = s[i + 1] + nums[i];
-
-        long long mini = LLONG_MAX;
-
-        for (int i = 0; i < n; i++) {
-            long long left = 1LL * nums[i] * i - (i > 0 ? p[i - 1] : 0);
-            long long right = (i < n - 1 ? s[i + 1] : 0) - 1LL * nums[i] * (n - i - 1);
-            long long cost = left + right;
-            mini = min(mini, cost);
-        }
-
-        return (int)mini;
+        int median = nums[n/2];
+        long long ans = 0;
+        for (int x : nums) ans += abs(x - median);
+        return ans;
     }
 };
