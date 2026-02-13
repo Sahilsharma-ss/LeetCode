d9ef732f3caaaf53f521cc87904ef2ac0643c273
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-02-13 19:54:11 +0530

    Time: 67 ms (61.58%), Space: 79.1 MB (52.3%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        vector<int> ans;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(auto i : nums)
+        {
+            pq.push(i);
+        }
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};
