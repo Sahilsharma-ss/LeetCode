4b50a79238a7f73230cd12e8cd246cb0bcec1817
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-06-25 01:28:06 +0530

    Time: 2746 ms (5.01%), Space: 713.1 MB (5.01%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,34 +1,80 @@
 constexpr int MOD = 1'000'000'007;
-
+typedef long long ll;
 class Solution {
 public:
+    int m;
+    int sz;
+    int dp[2001][2001][2];
+    int solve(int ind,int prev,int inc)
+    {
+        if(ind==sz) return 1;
+        if(dp[ind][prev][inc]!=-1) return dp[ind][prev][inc];
+        int res = 0;
+        if(inc==1)
+        {
+            for(int val = prev+1;val<=m;val++)
+            {
+                res  = (res + solve(ind+1,val,0))%MOD;
+            }
+        }
+        else
+        {
+            for(int val = 1;val<prev;val++)
+            {
+                res = (res + solve(ind+1,val,1))%MOD;
+            }
+        }
+       return dp[ind][prev][inc] = res%MOD;
+    }
     int zigZagArrays(int n, int l, int r) {
-        int m = r - l + 1;
-        vector<int> dp0(m, 0);
-        vector<int> dp1(m, 0);
-        vector<int> sum0(m + 1, 0);
-        vector<int> sum1(m + 1, 0);
-
-        for (int i = 0; i < m; i++) {
-            dp0[i] = dp1[i] = 1;
+        m = r - l + 1;
+        sz = n;
+        long long res =0;
+        int dp[2001][2001][2];
+    //     for(int val = 1;val<=m;val++)
+    //         {
+    //             res = (res + solve(1,val,1))%MOD;
+    //         }
+    //    for(int val = 1;val<=m;val++)
+    //         {
+    //             res = (res + solve(1,val,0))%MOD;
+    //         }
+    //         res = res%MOD;
+        // return (int)res;
+        for(int i =1 ;i<=m;i++)
+        {
+            dp[n][i][0] = 1;
+            dp[n][i][1] = 1;
         }
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                sum0[j + 1] = (sum0[j] + dp0[j]) % MOD;
-                sum1[j + 1] = (sum1[j] + dp1[j]) % MOD;
+            for(int i =n-1;i>=0;i--)
+            {
+                vector<ll>cs1(m+1,0);
+                vector<ll>cs2(m+1,0);
+                for(int nxt=1;nxt<=m;nxt++)
+                {
+                    cs1[nxt] = (cs1[nxt-1] + dp[i+1][nxt][0])%MOD;
+                    cs2[nxt] = (cs2[nxt-1] + dp[i+1][nxt][1])%MOD;
+                }
+                 for(int prev = 1;prev<=m;prev++)
+                    {
+                        //inc
+                        // for(int next = prev+1;next<=m;next++)
+                        // {
+                            dp[i][prev][1] = (cs1[m]-cs1[prev]+MOD)%MOD;
+                        // /}
+                        //dec
+                        // for(int next = 1;next<prev;next++)
+                        // {
+                            dp[i][prev][0] = cs2[prev-1];
+                        //}
+                    }
             }
-
-            for (int j = 0; j < m; j++) {
-                dp0[j] = (sum1[m] - sum1[j + 1] + MOD) % MOD;
-                dp1[j] = sum0[j];
+            for(int i =1;i<=m;i++)
+            {
+                res = (res+dp[1][i][1])%MOD;
+                res = (res+dp[1][i][0])%MOD;
             }
-        }
-
-        auto op = [](int acc, int x) { return (acc + x) % MOD; };
-        auto ans0 = std::reduce(dp0.begin(), dp0.end(), 0, op);
-        auto ans1 = std::reduce(dp1.begin(), dp1.end(), 0, op);
-
-        return (ans0 + ans1) % MOD;
+            return res;
     }
 };
