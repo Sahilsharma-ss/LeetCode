451a4258d13181b8c46a3aafe04b1cadbc743de9
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-07-12 00:49:11 +0530

    Time: 680 ms (6.19%), Space: 458.2 MB (5.01%) - LeetHub

--- 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.cpp
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.cpp
@@ -1,91 +1,140 @@
-class LCA {
-public:
-    LCA(const vector<vector<int>>& edges, const int root = 1) {
-        n = edges.size() + 1;
-        m = (log(n) / log(2)) + 1;
-        e.resize(n + 1);
-        d.resize(n + 1);
-        f.resize(n + 1, vector<int>(m, 0));
-        for (auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            e[u].push_back(v);
-            e[v].push_back(u);
-        }
-        dfs(root, 0);
-        for (int i = 1; i < m; i++) {
-            for (int x = 1; x <= n; x++) {
-                f[x][i] = f[f[x][i - 1]][i - 1];
-            }
-        }
-    }
-    void dfs(int x, int fa) {
-        f[x][0] = fa;
-        for (auto& y : e[x]) {
-            if (y == fa) {
-                continue;
-            }
-            d[y] = d[x] + 1;
-            dfs(y, x);
-        }
-    }
-
-    int lca(int x, int y) {
-        if (d[x] > d[y]) {
-            swap(x, y);
-        }
-        for (int i = m - 1; i >= 0; i--) {
-            if (d[x] <= d[f[y][i]]) {
-                y = f[y][i];
-            }
-        }
-        if (x == y) {
-            return x;
-        }
-        for (int i = m - 1; i >= 0; i--) {
-            if (f[y][i] != f[x][i]) {
-                x = f[x][i];
-                y = f[y][i];
-            }
-        }
-        return f[x][0];
-    }
-
-    int dis(int x, int y) { return d[x] + d[y] - d[lca(x, y)] * 2; }
-
-private:
-    int n;
-    int m;
-    vector<int> d;
-    vector<vector<int>> e;
-    vector<vector<int>> f;
-};
-
-const int MOD = 1e9 + 7;
-const int N = 100010;
-int p2[N];
-auto init = [] {
-    p2[0] = 1;
-    for (int i = 1; i < N; i++) {
-        p2[i] = p2[i - 1] * 2 % MOD;
-    }
-    return 0;
-}();
-
 class Solution {
 public:
-    vector<int> assignEdgeWeights(vector<vector<int>>& edges,
-                                  vector<vector<int>>& queries) {
-        LCA lca(edges, 1);
-        int m = queries.size();
-        vector<int> res(m);
-        for (int i = 0; i < m; i++) {
-            int x = queries[i][0];
-            int y = queries[i][1];
-            if (x != y) {
-                res[i] = p2[lca.dis(x, y) - 1];
+    int n;
+    long long mod = 1e9+7;
+    vector<int> depth,parent;
+
+    long long power(long long a,long long b)
+    {
+        long long ans = 1;
+        while(b)
+        {
+            if(b&1)
+            {
+                ans = (ans*a)%mod;
+            }
+            a = (a*a)%mod;
+            b>>=1;
+        }
+        return ans;
+    }
+
+    void dfs(int node ,unordered_map<int,vector<int>>&mp,int cnt,vector<int>&vis,int par)
+    {
+        if(vis[node]) return;
+
+        vis[node] = 1;
+        depth[node] = cnt;
+        parent[node] = par;
+
+        for(auto i : mp[node])
+        {
+            if(!vis[i])
+            {
+                dfs(i,mp,cnt+1,vis,node);
             }
         }
+    }
+
+    vector<int> assignEdgeWeights(vector<vector<int>>& edges, vector<vector<int>>& queries) {
+
+        unordered_map<int,vector<int>> mp;
+        n = 0;
+
+        for(auto i : edges)
+        {
+            n = max({n,i[0],i[1]});
+            mp[i[0]].push_back(i[1]);
+            mp[i[1]].push_back(i[0]);
+        }
+
+        depth.resize(n+1,0);
+        parent.resize(n+1,-1);
+
+        vector<int> vis(n+1,0);
+
+        dfs(1,mp,0,vis,-1);
+
+        int cols = log2(n)+1;
+
+        vector<vector<int>> up(n+1,vector<int>(cols,-1));
+
+        for(int i=1;i<=n;i++)
+        {
+            up[i][0] = parent[i];
+        }
+
+        for(int j=1;j<cols;j++)
+        {
+            for(int node=1;node<=n;node++)
+            {
+                if(up[node][j-1]!=-1)
+                {
+                    up[node][j] = up[up[node][j-1]][j-1];
+                }
+            }
+        }
+
+        vector<int> res;
+
+        for(auto i : queries)
+        {
+            int a = i[0];
+            int b = i[1];
+
+            int u = depth[a];
+            int v = depth[b];
+
+            if(u<v)
+            {
+                swap(a,b);
+                swap(u,v);
+            }
+
+            int k = u-v;
+
+            for(int j=0;j<cols;j++)
+            {
+                if(k&(1<<j))
+                {
+                    a = up[a][j];
+                }
+            }
+
+            int lcanode;
+
+            if(a==b)
+            {
+                lcanode = a;
+            }
+            else
+            {
+                for(int j=cols-1;j>=0;j--)
+                {
+                    if(up[a][j]==-1) continue;
+
+                    if(up[a][j]!=up[b][j])
+                    {
+                        a = up[a][j];
+                        b = up[b][j];
+                    }
+                }
+
+                lcanode = parent[a];
+            }
+
+            int d = u + v - 2*depth[lcanode];
+
+            if(d==0)
+            {
+                res.push_back(0);
+            }
+            else
+            {
+                res.push_back(power(2,d-1));
+            }
+        }
+
         return res;
     }
 };
