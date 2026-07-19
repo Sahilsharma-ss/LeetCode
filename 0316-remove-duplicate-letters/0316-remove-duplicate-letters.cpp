c9924769fcfaf327bce0995c95d77a8e8d8c3aa4
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-07-19 13:04:10 +0530

    Time: 2 ms (25.96%), Space: 10 MB (7.86%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int>freq(26,0);
+        vector<int>used(26,0);
+        stack<int>stk;
+        for(auto i : s)
+        {
+            freq[i-'a']++;
+        }
+
+        for(auto c : s)
+        {
+            int idx = c-'a';
+            freq[idx]--;
+            if(used[idx]) continue;
+            while(!stk.empty() && stk.top()>c && freq[stk.top()-'a']>0) // if we get smaller and element in stk  & have another occurence take it greedly
+            {
+                used[stk.top()-'a'] = 0; // unuse the element 
+                stk.pop();
+            }
+            stk.push(c);
+            used[c-'a'] = 1;
+        }
+        string ans = "";
+        while(!stk.empty())
+        {
+            ans+=stk.top();
+            stk.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
