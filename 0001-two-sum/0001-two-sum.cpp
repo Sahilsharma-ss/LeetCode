6b2337cb3495b529aeffda2da815277703e41a37
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-08-09 01:20:16 +0530

    Time: 0 ms (100%), Space: 15 MB (23.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,20 +4,14 @@ public:
         // unordered_map [{2,0},{7,1},{11,2},{15,3}]
         unordered_map<int,int>mp; // {value,index}
         int n = nums.size();
-        for(int i=0;i<n;i++) // O(n)
-        {
-            mp[nums[i]] = i;
-        }
         for(int i =0;i<n;i++) // tc : O(N);
         {
             int tar = target-nums[i];
             if(mp.find(tar)!=mp.end())
             {
-                if(i != mp[tar])
-                {
-                    return {i,mp[tar]};
-                }
+                return {mp[tar],i};
             }
+            mp[nums[i]] = i;
         }
         return {};
     }
