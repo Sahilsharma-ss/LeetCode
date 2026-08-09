25f3e5b845c957796c18c1ccc19e0f811b1db1e9
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-08-09 22:16:47 +0530

    Time: 5 ms (8.89%), Space: 40.1 MB (96.3%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,51 +1,54 @@
 class Solution {
 public:
-    int productwithoutzero(vector<int>&nums)
-    {
-        int prod=1;
-        for(auto i : nums)
-        {
-            if(i!=0)
-            {
-                prod*=i;
-            }
-        }
-        return prod;
-    }
     vector<int> productExceptSelf(vector<int>& nums) {
-        int prod=1;
-        int n=nums.size();
-        bool zero=false;
-        int zerocnt=0;
-        vector<int>haszero(n,0);
-        for(int i=0;i<n;i++)
+        int n = nums.size();
+        // vector<int>pref(n),suff(n);
+        // pref[0] = nums[0];
+        // for(int i=1;i<nums.size();i++)
+        // {
+        //     pref[i] = pref[i-1]*nums[i];
+        // }
+        // suff[n-1] = nums[n-1];
+        // for(int i=n-2;i>=0;i--)
+        // {
+        //     suff[i] = suff[i+1]*nums[i];
+        // }
+
+        // vector<int>ans;
+        // for(int i =0;i<n;i++) //   1 2 3 4 
+        // {
+        //     int left = (i-1)>=0?pref[i-1]:1;
+        //     int right = (i+1)<n?suff[i+1]:1;
+        //     ans.push_back(left*right);
+        // }
+
+        int zero = 0;
+        int mul = 1;
+        int ind = 0;
+        for(int i =0;i<n;i++)
         {
-            prod*=nums[i];
             if(nums[i]==0)
             {
-                zero=true;
-                haszero[i]=1;
-                zerocnt++;
+                ind = i;
+                zero++;
+                continue;
             }
+            mul*=nums[i];
         }
-        vector<int>res(n,0);
-        if(!zero)
+        vector<int>ans(n,0);
+        if(zero>=2)
         {
-            for(int i=0;i<n;i++)
-            {
-                res[i]=prod/nums[i];
-            }
+            return ans;
         }
-        else if(zerocnt==1)
+        else if(zero==1)
         {
-            for(int i=0;i<n;i++)
-            {
-                if(haszero[i]==1)
-                {
-                    res[i]=productwithoutzero(nums);
-                }
-            }
+            ans[ind] = mul;
+            return ans;
         }
-        return res;
+        for(int i =0;i<n;i++)
+        {
+            ans[i] = mul/nums[i];
+        }
+        return ans;
     }
 };
