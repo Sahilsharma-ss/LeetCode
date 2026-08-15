12dde7de58283192a6350750bb957f9947882121
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-08-15 14:48:51 +0530

    Time: 10 ms (24.11%), Space: 21.2 MB (5.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
+    bool iscycle(int node,unordered_map<int,vector<int>>&mp,vector<int>&vis,vector<int>&pathvis)
+    {
+        vis[node] = 1;
+        pathvis[node] =1;
+        for(auto i : mp[node])
+        {
+            if(!vis[i])
+            {
+                if(iscycle(i,mp,vis,pathvis)) return true; 
+            }
+            else
+            {
+               if(pathvis[i]==1)
+               {
+                    return true;
+               }
+            }
+        }
+        pathvis[node] = 0;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n=numCourses;
-        vector<vector<int>>adj(n);
-        vector<int>indeg(n,0);
-        for(auto &i : prerequisites)
+        unordered_map<int,vector<int>>mp;
+        for(auto i : prerequisites)
         {
-            int u=i[0];
-            int v=i[1];
-            adj[u].push_back(v);
-            indeg[v]++;
+            mp[i[1]].push_back(i[0]);
         }
-        queue<int>q;
-        for(int i=0;i<n;i++)
+        vector<int>vis(numCourses,0),pathvis(numCourses,0);
+        for(int i=0;i<numCourses;i++)
         {
-            if(indeg[i]==0)
+            if(vis[i]) continue;
+            if(iscycle(i,mp,vis,pathvis))
             {
-                q.push(i);
+                return false;
             }
         }
-        vector<int>sort;
-        while(!q.empty())
-        {
-            int node=q.front();
-            sort.push_back(node);
-            q.pop();
-
-            for(int i : adj[node])
-            {
-                indeg[i]--;
-                if(indeg[i]==0)
-                {
-                    q.push(i);
-                }
-            }
-        }
-        if(sort.size()!=numCourses) return false;
         return true;
     }
 };
