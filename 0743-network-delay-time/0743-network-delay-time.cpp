167c17c6b4055efeb6b6b926ab419afc33d030c1
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-08-16 00:31:26 +0530

    Time: 100 ms (12.52%), Space: 47.9 MB (30.83%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        const int inf = 1e9;
-        vector<vector<pair<int, int>>> adj(n + 1); 
-        for (auto &edge : times) {
-            int u = edge[0], v = edge[1], w = edge[2];
-            adj[u].push_back({v, w});
+        vector<int>dist(n+1,1e9);
+        unordered_map<int,vector<pair<int,int>>>mp;
+        for(auto i : times)
+        {
+            mp[i[0]].push_back({i[1],i[2]});
         }
-
-        // Step 2: Dijkstra
-        vector<int> dist(n + 1, inf);
-        vector<bool> visited(n + 1, false);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
         dist[k] = 0;
-        pq.push({0, k});
-
-        while (!pq.empty()) {
+        vector<int>vis(n+1,0);
+        while(!pq.empty())
+        {
             int node = pq.top().second;
+            int wt = pq.top().first;
+            vis[node] = 1;
             pq.pop();
-            if (visited[node]) continue;
-            visited[node] = true;
-
-            for (auto &[neighbor, weight] : adj[node]) {
-                if (!visited[neighbor] && dist[node] + weight < dist[neighbor]) {
-                    dist[neighbor] = dist[node] + weight;
-                    pq.push({dist[neighbor], neighbor});
+            for(auto i : mp[node])
+            {
+                if(!vis[i.first])
+                {
+                    if(wt+i.second < dist[i.first])
+                    {
+                        dist[i.first] = wt+i.second;
+                        pq.push({wt+i.second,i.first});
+                    }
                 }
             }
         }
-
-        int ans = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (dist[i] == inf) return -1;
-            ans = max(ans, dist[i]);
-        }
-        return ans;
+        dist[0] = 0;
+        int maxi = *max_element(dist.begin(),dist.end());
+        return maxi==1e9?-1:maxi;
     }
 };
