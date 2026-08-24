75c1d63e0291c0d1a29ea4b1152367f7f65371f1
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-08-25 00:27:02 +0530

    Time: 4 ms (23.39%), Space: 112.6 MB (26.13%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int sumg=0;
-        for(auto i : gas)
-        {
-            sumg += i;
-        }
-        int sumc=0;
-        for(auto i : cost)
-        {
-            sumc+=i;
-        }
-        if(sumg<sumc) return -1;
-        int total =0;
-        int result = 0;
-        for(int i =0;i<gas.size();i++)
-        {
-            total += (gas[i]-cost[i]);
-            if(total<0) 
+       // unique sol or no solution  it means there will be only one valid index
+       // from where i can complete the journey circularly 
+
+       int n = cost.size();
+       int tot_cost = 0;  // if tot_cost < 0 it means no ans ret -1
+
+       int extra_cost = 0;
+       int startInd =0;
+       for(int i=0;i<n;i++)
+       {
+            tot_cost += gas[i] - cost[i];
+            extra_cost += gas[i] - cost[i];
+            if(extra_cost<0)
             {
-                total =0;
-                result= i+1;
+                extra_cost = 0;
+                startInd = i+1;
             }
-        }
-        return result;
+       }
+       return tot_cost<0?-1:startInd;
     }
 };
