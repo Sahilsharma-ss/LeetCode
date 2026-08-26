010f49e5e00c4574369cda14cdb13ec87e5ef10d
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2026-08-26 16:41:32 +0530

    Time: 0 ms (100%), Space: 95 MB (94.82%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>vec=nums1;
-        vec.insert(vec.end(),nums2.begin(),nums2.end());
-        sort(vec.begin(),vec.end());
-        int size = vec.size();
-        if(size%2==0)
+        if(nums1.size()>nums2.size())
         {
-            return (double) (vec[(double)size/2]+vec[(double)size/2-1])/2;
+            return findMedianSortedArrays(nums2,nums1);
         }
-        else
+        int n = nums1.size();
+        int m = nums2.size();
+        int l =0;
+        int r =n;
+        while(l<=r)
         {
-            return (double) vec[size/2];
+            int px = l+(r-l)/2;
+            int py = (m+n+1)/2 - px;
+            //left
+            int x1 = (px==0)?-1e9:nums1[px-1];
+            int x2 = (py==0)?-1e9:nums2[py-1];
+            //right
+            int x3 = (px==n)?1e9:nums1[px];
+            int x4 = (py==m)?1e9:nums2[py];
+
+            if(x1<=x4 && x2<=x3)
+            {
+                if((m+n)%2==0)
+                {
+                    return (max(x1,x2) + min(x3,x4))/2.0;
+                }
+                else
+                {
+                    return max(x1,x2);
+                }
+            }
+
+            if(x1>x4)
+            {
+                r = px-1;
+            }
+            else
+            {
+                l = px+1;
+            }
         }
+        return -1;
     }
 };
